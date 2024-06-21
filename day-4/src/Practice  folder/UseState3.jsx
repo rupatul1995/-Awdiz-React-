04cd440c14123781e2ad1602b9c7c85c2538e920
rupatul1995 <rupalichavan578@gmail.com>
2024-06-21 20:34:55 +0530

    updata

--- day-4/src/Practice  folder/UseState3.jsx
+++ day-4/src/Practice  folder/UseState3.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
-
+import {useNavigate} from "react-router-dom";
 
 function UseState3(){
     const[counter, setCounter]=useState(1);
-    const[counter1, setCounter1]=useState(1);
+    const[counter1, setCounter1]=useState(10);
+    const router =useNavigate()
+    // const[isUserLoggedIn, setIsUseLoggedIn]=useState(true);
+    
+
+    // function mydata(){
+    //     setIsUseLoggedIn((prevState)=>!prevState);
+    // }
+
 function Increment(){
-    if(counter>10){
-        setCounter(counter+1);
-    }
-   
+    setCounter(counter+1);
 }
 function Decrement(){
     if(counter>0){
@@ -22,10 +27,14 @@ function Reset(){
 
 
 function Increment1(){
-    setCounter1(counter1+1);
+    if(counter1>=10){
+        setCounter1(counter1+1);
+    }
+   
+
 }
 function Decrement1(){
-    if(counter1>0){
+    if(counter1>10){
         setCounter1(counter1-1);
     }
     
@@ -33,12 +42,16 @@ function Decrement1(){
 function Reset1(){
     setCounter1(10);
 }
+
+// useEffect(()=>{alert("CALLING FROM USEeFFECT")},[counter]);
     return(
 <div>
+    {/* {isUserLoggedIn? <button onClick={mydata}>Welcome !</button>:<button onClick={mydata}>Please Login</button>} */}
     <h1>counter:{counter}</h1>
     <button onClick={Increment}>+</button>
     <button onClick={Decrement}>-</button>
     <button onClick={Reset}>Reset</button>
+ 
 
 
 
@@ -46,6 +59,7 @@ function Reset1(){
     <button onClick={Increment1}>+</button>
     <button onClick={Decrement1}>-</button>
     <button onClick={Reset1}>Reset</button>
+    <button onClick={()=>router("/Login")}>Go to home page</button>
     </div>
     );
 }
