96c2d43e2b5c0d2fe44d26087237344320eca5b7
rupatul1995 <rupalichavan578@gmail.com>
2024-07-19 14:22:29 +0530

    update

--- day-4/src/components/Home.jsx
+++ day-4/src/components/Home.jsx
@@ -3,19 +3,39 @@ import { useNavigate } from "react-router-dom";
 
 import { CounterContext } from "../15-06-24/Counter.Context";
 import { themeContext } from "../18-06-24/theme.context";
+import Api from "../axiosConfig";
+import toast from "react-hot-toast";
 
 function Home(){
     const {state}=useContext(CounterContext);
     const{themeState,dispatch}=useContext(themeContext);
 const router=useNavigate();
+
+ async function handleLogout(){
+    try{
+        const response =await Api.post("/auth/logout");
+        if(response.data.success){
+            dispatch({type:"LOGOUT"});
+            router("/login");
+            toast.success(response.data.message);
+        }else{
+            toast.error("Logout failed");
+        }
+    }catch(error){
+        toast.error("Failed to Logout"); 
+
+    }
+ }
+
     return (        
     <div style={{backgroundColor:themeState.theme ==="LIGHT"?"yellow":"grey",}}>
       
     <h1>Home page:-{state?.counter }</h1>
     {/* <h1>Home page:-{state?.user?.name }</h1> */}
+    <button onClick={handleLogout}>Logout</button>
     <button onClick={()=>router("/counter")}> Go to Counter</button>
     <button onClick={()=>router("/useNavigate")}>Redirect to Routing page</button>
-    <button onClick={()=>dispatch({type:themeState?.theme==="LIGHT"? "DARK":"LIGHT"})}> change{themeState?.theme==="LIGHT"? "LIGHT":"DARK"} theme</button>
+    <button onClick={()=>dispatch({type:themeState?.theme==="LIGHT"? "DARK":"LIGHT"})}> change{themeState?.theme==="LIGHT"? "LIGH>T":"DARK"} theme</button>
     </div>
     );
 }
